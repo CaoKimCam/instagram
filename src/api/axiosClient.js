32d5d3ca80c277deadacc96bd87ae4e6e6cb4b35
CaoKimCam <21520641@gm.uit.edu.vn>
2024-07-06 23:44:46 +0700

    up

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://photos-sharing-website-backend-0e896e8a6dc9.herokuapp.com',
   headers: {
     'Content-Type': 'application/json',
   },
